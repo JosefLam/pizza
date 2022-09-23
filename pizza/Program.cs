3cbb22bfe38a8846b47cd9a242eefeb8b0686d2e
Josef Lambley <17LamblJ@fulford.york.sch.uk>
2022-09-23 15:20:31 +0100

    started program

--- pizza/Program.cs
+++ pizza/Program.cs
@@ -10,6 +10,39 @@ namespace pizza
     {
         static void Main(string[] args)
         {
+            List<string> options = new List<string> {
+                "Pepperoni", "margherita","Ham and Pineapple"
+            };
+
+            string selection = ChooseFrom(options);
+        }
+
+        private static string ChooseFrom(List<string> options)
+        {
+            //give options
+            bool valid = false;
+            while (!valid)
+                for (int i = 0; i < options.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {options[i]}");
+                    Console.Write($"Choose a number (1-{options.Count}");
+                    string response = Console.ReadLine();
+                    int chosenposition = -1;
+                    if (int.TryParse(response, out chosenposition) &&
+                        chosenposition > 0 &&
+                        chosenposition <= options.Count)
+                    {
+                        valid = true;
+                        return options[chosenposition - 1];
+                    }
+
+                    return options[chosenposition - 1];
+                }
+
+            //repeat until valid input
+
+           
+            
         }
     }
 }
